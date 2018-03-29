3e3197c47ff6649caf9d015b3042a3744a7d133e
Bharath Kumar R <BharathKumarRavichandran@users.noreply.github.com>
2018-03-29 19:22:57 +0530

    All elements aligned and displayed

--- game.js
+++ game.js
@@ -1,37 +1,48 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-var canvasWidth = 1100;
-var canvasHeight = 600;
+var canvasWidth = 1300;
+var canvasHeight = 650;
 var spriteWidth = 1400;
 var spriteHeight = 770;
+var baseWidth = 336;
+var baseHeight = 112;
+var cartWidth = 100;
+var cartHeight = 100;
+var charX = 60;
+var charY = canvasHeight-baseHeight-80;
+var cartX = 400;
+var cartY = charY+45;
 var rows=2;
-var cols=8;
-var width = spriteWidth/cols;
-var height = spriteHeight/rows;
+var cols=5;
+var charWidth = spriteWidth/cols;
+var charHeight = spriteHeight/rows;
 var curFrame=0;
 var frameCount;
 var x=0;
 var y=0;
 var srcX=0;
 var srcY=0;
+var score = 0;
 
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 
+var base = new Image();
 var bg = new Image();
+var bg1 = new Image();
+var bg2 = new Image();
 var character = new Image();
 var cloud = new Image();
 var hayCart = new Image();
-var hayBundle = new Image();
-var hayFork = new Image();
 
+base.src = "sprites/base.png";
 bg.src = "sprites/bg.jpg";
+bg1.src= "sprites/sand_brick.png";
+bg2.src = "sprites/dark_bricks.png";
 character.src = "sprites/character.png";
 cloud.src = "sprites/cloud.png";
-hayCart = "sprites/Hay_Cart.png";
-hayBundle = "sprites/Heuballen.png";
-hayFork = "sprites/Ferskt_hay.png";
+hayCart.src = "sprites/Hay_Cart.png";
 
 
 var dead = new Audio("audio/dead.wav");
@@ -39,11 +50,16 @@ var gameOver = new Audio("audio/gameOver.mp3");
 var jump = new Audio("audio/jump.wav");
 
 
+
 function stopAudio(audio) {    //Function to stop audio the current audio from playing
     audio.pause();
     audio.currentTime = 0;
 }
 
-
-ctx.drawImage(bg,0,0,canvasWidth,canvasHeight);
-ctx.drawImage(hayCart);
+var pat1 = ctx.createPattern(bg1,"repeat");
+ctx.rect(0,0,canvasWidth,canvasHeight-baseHeight+50);
+ctx.fillStyle = pat1;
+ctx.fill();
+ctx.drawImage(base,0,canvasHeight-baseHeight+50,canvasWidth,100);
+ctx.drawImage(character,srcX,srcY,charWidth,charHeight,charX,charY,150,150);
+ctx.drawImage(hayCart,cartX,cartY,cartWidth,cartHeight);
