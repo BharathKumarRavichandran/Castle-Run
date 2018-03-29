7d86bbe05ef0354a236914d42a49e5462d8aa69c
Bharath Kumar R <BharathKumarRavichandran@users.noreply.github.com>
2018-03-29 12:20:00 +0530

    Files arranged in  respective folders
    Audio files and sprite files arranged in their respective folders

--- game.js
+++ game.js
@@ -6,7 +6,9 @@ var canvasHeight = 600;
 var spriteWidth = 1400;
 var spriteHeight = 770;
 var rows=2;
-var cols=5;
+var cols=8;
+var width = spriteWidth/cols;
+var height = spriteHeight/rows;
 var curFrame=0;
 var frameCount;
 var x=0;
@@ -16,7 +18,7 @@ var srcY=0;
 
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
-
+/*
 var bg = new Image();
 bg.src = "bg.jpg";
 var character = new Image();
@@ -26,7 +28,9 @@ cloud.src = "cloud.png";
 var pipeGreen = new Image();
 pipeGreen.src = "pipe-green.png";
 var pipeRed = new Image();
-pipeRed.src = "pipe-red.png";
+pipeRed.src = "pipe-red.png";*/
+var test = new Image();
+test.src = "character.png";/*
 
 var gameOver = new Audio("gameOver.mp3");
 var jump = new Audio("jump.wav");
@@ -36,10 +40,21 @@ function stopAudio(audio) {    //Function to stop audio the current audio from p
     audio.currentTime = 0;
 }
 
-ctx.drawImage(bg,x,y,canvasWidth,canvasHeight);
+ctx.drawImage(test,srcX,srcY,width,height,x,y,width,height);
+//ctx.drawImage(bg,0,0,canvasWidth,canvasHeight);
 
-alert("hello");
+alert("hello...testing to check errors");
 
 function updateFrame(){
+
+test.onload = function() {
+ ctx.drawImage(test,100,100);
+}*/
+
+function draw(){
+	ctx.drawImage(test,srcX,srcY,width,height,x,y,width,height);	
+}
+
+setInterval(draw,100);
 
 }
